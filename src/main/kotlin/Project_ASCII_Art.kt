9ee9dfca29d98ed262277a4ac00c593f57b98191
alxmart <alxmartinez@gmail.com>
2022-09-05 15:24:23 -0300

    Adding exercises from lessons.

---
+++ src/main/kotlin/Project_ASCII_Art.kt
@@ -0,0 +1,14 @@
+// Write your code below
+fun main() {
+
+    println("     /---------------\\        ")
+    println("    /                 \\       ")
+    println("   /                   \\      ")
+    println("  |---------------------|      ")
+    println("  |                     |      ")
+    println("  |    |----|    |----| |      ")
+    println("  |    |____|    |    | |      ")
+    println("  |              |    | |      ")
+    println("  |______________|____|_|_     ")
+
+}
