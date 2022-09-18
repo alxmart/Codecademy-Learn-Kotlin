225e35f174271c48073049af3b678f84768b8d88
alxmart <alxmartinez@gmail.com>
2022-09-18 11:37:52 -0300

    Adding exercises from lessons.

---
+++ src/main/kotlin/ChallengeIncrementDecrement.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    var a = 5
+    a += 6
+    // a: 11
+    println(a)
+
+    var b = a * 9
+    var c = b / 3
+    c++
+    // c: 34
+    println(c)
+
+    var d = c + 8
+    d %= 4
+    d--
+    // d: 1
+    println(d)
+
+}
