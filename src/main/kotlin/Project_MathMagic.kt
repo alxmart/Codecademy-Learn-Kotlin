c508e75681a7ae15580fe45fff443a09961ab91c
alxmart <alxmartinez@gmail.com>
2022-09-18 12:48:17 -0300

    Adding exercises from lessons.

---
+++ src/main/kotlin/Project_MathMagic.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val userName: String = "Alex"
+    var magicNum: Int
+    magicNum = 5 * 5
+    magicNum += 3
+    magicNum *= 2
+    magicNum += 1
+    magicNum -= 6
+    println(" $userName's age is $magicNum")
+}
