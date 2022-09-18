38cb091f3bb919166c2267e5d9a75640b0a0de4f
alxmart <alxmartinez@gmail.com>
2022-09-18 14:57:55 -0300

    Adding exercises from lessons.

---
+++ src/main/kotlin/GarbageDay.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var isTuesday = true
+    // Write your code below
+    if (isTuesday) {
+        println("Take out the trash.")
+    }
+}  
---
+++ src/main/kotlin/OrderOfConditionalExpression.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var expressionValue: Boolean
+
+    expressionValue = false
+
+    if (!true && (false && true || !false)) {
+        println("Hello")
+    } else {
+        println("Goodbye")
+    }
+
+    // Write your code below
+    println(expressionValue)
+
+
+}
