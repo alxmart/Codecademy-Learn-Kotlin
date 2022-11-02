5599821b354cce31bfdd25e4160bff7a36b1a706
alxmart <alxmartinez@gmail.com>
2022-11-02 11:02:07 -0300

    Adding exercises from lessons.

--- src/main/kotlin/Pi.kt
+++ src/main/kotlin/Pi.kt
@@ -1,6 +1,7 @@
 fun main() {
     // Write your code below
     val pi: Double = 3.14
+    println(pi)
 
     //pi = 3.22 // error: val cannot be assigned
 
---
+++ src/main/kotlin/reviewDataTypes.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // Write your code below
+    var nameSong : String = "The Final Countdown"
+    var duration: String = "04:32"
+    var listeningSong : Boolean = false
+    println(nameSong)
+    println(duration)
+    println(listeningSong)
+}
