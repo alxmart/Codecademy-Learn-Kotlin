b149d7077bc558cb2d0485d3c2a1ec60d7896fbe
alxmart <alxmartinez@gmail.com>
2022-12-08 14:28:19 -0300

    Adding exercises from lessons.

--- src/main/kotlin/Project_Animal_Sanctuary.kt
+++ src/main/kotlin/Project_Animal_Sanctuary.kt
@@ -1,10 +1,15 @@
 fun main() {
     // Write your code below 🏞
 
-    var responsibilities = mutableListOf("feed the chimps", "play a random game", "conduct a health check on Foxie")
+    var responsibilities = mutableListOf("feed the chimps",
+        "play a random game", "conduct a health check on Foxie")
+
     var responsibilitiesComplete = 0
+
     var timeSpent = 0
+
     var totalShiftTime = 4
+
     var foxiesHealthCheck = mutableMapOf<String, Any?>()
 
     var chimpHaveEaten = mutableMapOf("Bonnie" to false, "Jubilee" to false, "Frodo" to false, "Foxie" to false)
@@ -33,35 +38,49 @@ fun main() {
 
     println ("Next, ${responsibilities[1]}")
 
-    var games = mutableSetOf("tug-of-war with a blanket", "catch and throw", "number game")
+    var games = mutableSetOf("tug-of-war with a blanket",
+                             "catch and throw", "number game")
+
     var randomGame = games.random()
 
     println (randomGame)
+
     timeSpent++
+
     responsibilitiesComplete ++
 
-   println("Each animal has now played a game of $randomGame! You've completed $responsibilitiesComplete / ${responsibilities.size} responsibilities.")
+   println("Each animal has now played a game of $randomGame! You've " +
+           "completed $responsibilitiesComplete / ${responsibilities.size}" +
+           " responsibilities.")
 
     //THIRD RESP
 
      println ("Next, ${responsibilities[2]}")
 
     foxiesHealthCheck.put("temperature", 32.7)
+
     foxiesHealthCheck.put("mood", "happy")
 
-    println ("Foxie has a temperature of ${foxiesHealthCheck["temperature"]} and is feeling  ${foxiesHealthCheck["mood"]}")
+    println ("Foxie has a temperature of ${foxiesHealthCheck["temperature"]} " +
+            "and is feeling  ${foxiesHealthCheck["mood"]}")
 
     timeSpent++
+
     responsibilitiesComplete ++
 
-    println("You've now completed ${responsibilitiesComplete} / ${responsibilities.size} responsibilities.")
+    println("You've now completed ${responsibilitiesComplete} / " +
+            "${responsibilities.size} responsibilities.")
 
-    if (timeSpent <= totalShiftTime && responsibilitiesComplete == responsibilities.size) {
-           println("\nAwesome work! You've completed all of your responsibilites for the day, and you can clock out.")
-    } else if (timeSpent >= totalShiftTime && responsibilitiesComplete == responsibilities.size) {
-                println("Great job today! You've completed all of your responsibilities but with over time.")
+    if (timeSpent <= totalShiftTime &&
+        responsibilitiesComplete == responsibilities.size) {
+           println("\nAwesome work! You've completed all of your" +
+                   " responsibilites for the day, and you can clock out.")
+    } else if (timeSpent >= totalShiftTime &&
+        responsibilitiesComplete == responsibilities.size) {
+                println("Great job today! You've completed all of your" +
+                        " responsibilities but with over time.")
     } else {
-         println("You went over time and did not complete all of your responsibilites.")
+         println("You went over time and did not complete all of your" +
+                 " responsibilites.")
     }
-
 }
